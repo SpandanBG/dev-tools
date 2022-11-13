117b35ae6b39f13f0bc445ec93941953659064cb
SpandanBG <spandanbg@gmail.com>
2022-11-13 14:30:43 +0530

    project init

---
+++ src/core/_index.tsx
@@ -0,0 +1,7 @@
+import * as ReactDOM from 'react-dom';
+
+const Index = () => {
+  return <div>Hello, world</div>;
+}
+
+ReactDOM.render(<Index />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = [
+  {
+    mode: 'development',
+    entry: './src/core/_app.ts',
+    target: 'electron-main',
+    module: {
+      rules: [{
+        test: /\.ts$/,
+        include: /src/,
+        use: [{ loader: 'ts-loader' }]
+      }]
+    },
+    output: {
+      path: __dirname + '/dist',
+      filename: 'app.js'
+    },
+  },{
+    mode: 'development',
+    entry: './src/core/_index.tsx',
+    target: 'electron-renderer',
+    devtool: 'source-map',
+    module: {
+      rules: [{
+        test: /\.ts(x?)$/,
+        include: /src/,
+        use: [{ loader: 'ts-loader' }]
+      }]
+    },
+    output: {
+      path: __dirname + '/dist',
+      filename: 'common.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/core/_document.html',
+        filename: '_document.html'
+      })
+    ]
+  }
+];
